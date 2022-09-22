b803bdac32378393ec6bb970b7aeb3a4d4531d5c
ShakoRou <113183312+ShakoRou@users.noreply.github.com>
2022-09-22 05:35:23 +0200

    first commit

---
+++ day1.1/lessen1.1.py
@@ -0,0 +1,12 @@
+from time import process_time_ns
+
+
+name ="shako"
+surname="rou"
+age="27"
+height="170"
+print(name)
+print(surname)
+print(age)
+print(height)
+print(name+ "  " + surname + "  " +str(age)+ "  " +str(height))
